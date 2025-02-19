12f765a3c92c47e49a608a7f57a29839bfd2c488
Alberto Cruz <xpiradesign@gmail.com>
2025-02-19 17:12:44 +0100

    maquetacion servicio y header movil

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,24 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
+// @author: Alberto Cruz
+// @desc: Lógica de toogle con clase .expanded sobre menu navegación móvil al hacer click en botón hamburguesa
+
+const hamburger = document.getElementById("hamburger-button");
+const nav = document.getElementById("menu-mobile");
+
+if (hamburger && nav) {
+    hamburger.addEventListener("click", () => {
+        nav.classList.toggle("expanded");
+    });
+}
+
+// Agregar evento a cada menú con submenú
+document.querySelectorAll(".menu-item").forEach((menuItem) => {
+  menuItem.addEventListener("click", (event) => {
+    event.preventDefault(); // Evita que el enlace navegue a "#"
+
+    const submenu = menuItem.querySelector(".submenu");
+
+    if (submenu) {
+      submenu.classList.toggle("expanded");
+    }
+  });
 });
