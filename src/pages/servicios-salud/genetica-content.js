33e9d9bf1a3c05ca6447cbf10787379654842efc
Alberto Cruz <xpiradesign@gmail.com>
2025-03-05 12:13:53 +0100

    Parametrizando pagina genetica con columnas

---
+++ src/pages/servicios-salud/genetica-content.js
@@ -0,0 +1,265 @@
+export const content = [
+  {
+    component: "HeroSmall",
+    pageTitle: "Genética",
+    imageSrc: "/src/assets/genetica/banner-genetica1.jpg",
+  },
+  {
+    component: "IconRow",
+    imageSrc: "/src/assets/genetica/genetica-icon1.png",
+    title: "Nutrición Personalizada",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon1.png",
+    title: "Intolerancias Alimentarias",
+    text: "El test de intolerancia alimentaria A200 evalúa la respuesta del sistema inmune frente a más de 200 alimentos habituales en la dieta mediterránea.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon2.png",
+    title: "Nutrigenética",
+    text: "Personas que deseen adaptar su dieta en función a su genética y conocer sus necesidades nutricionales específicas de manera personalizada.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon3.png",
+    title: "MigraTest",
+    text: "Indicada en el caso de personas que padezcan migrañas y quieran saber si el origen está relacionado con un déficit de la actividad de la enzima DAO.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon4.png",
+    title: "Cuidado intestinal",
+    text: "Lactosa Test consiste en un estudio genético de intolerancia a la lactosa, a través de una muestra de raspado bucal.",
+  },
+  {
+    component: "IconRow",
+    imageSrc: "/src/assets/genetica/genetica-icon2.png",
+    title: "Cardio",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon5.png",
+    title: "CardioScore",
+    text: "Se basa en una extracción de sangre que permitirá una evaluación clínico-genética del riesgo cardiovascular real.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon6.png",
+    title: "Omega 3",
+    text: "Indicador para la prevención de enfermedades cardiovasculares y cerebrovasculares.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon7.png",
+    title: "Perfil Cardiovascular",
+    text: "Permite obtener información sobre los niveles de colesterol (HDL y LDL), triglicéridos, homocisteína, proteína C reactiva, índice aterogénico, lipoproteína (a) y hemoglobina glicosilada..",
+  },
+  {
+    component: "IconRow",
+    imageSrc: "/src/assets/genetica/genetica-icon3.png",
+    title: "Edad y Salud",
+  },
+  {
+    component: "ImageRow",
+    imageSrc: "/src/assets/genetica/genetica1.png",
+    html: `
+    <h3>Longitud Telomérica</h3>
+    <p>
+      Te permitirá conocer la edad biológica de tu organismo frente a tu edad
+      cronológica. Si la prueba resulta desfavorable, la incorporación de
+      hábitos de vida saludables te ayudará a mejorar tu longitud telomérica y
+      edad biológica en el plazo de un año.
+    </p><br />
+    <h3>Alteraciones del Sueño</h3>
+    <p>
+      Permite determinar los niveles de melatonina en saliva en diferentes
+      momentos de la noche pudiendo detectar cualquier desviación respecto al
+      nivel normal, tanto en horarios como en intensidad. Con ello, se puede
+      observar si la producción de melatonina es o no suficiente para la
+      conciliación del sueño.
+    </p><br />
+    <h3>Valoración del Estrés</h3>
+    <p>
+      Comparar los niveles de cortisol y DHEA con los valores normales,
+      aportando información acerca de la fase de estrés en la que se encuentra
+      el individuo.
+    </p>`,
+  },
+  {
+    component: "IconRow",
+    imageSrc: "/src/assets/genetica/genetica-icon4.png",
+    title: "Bienestar – Prevención",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon8.png",
+    title: "Prevención Check",
+    text: "Personas que deseen mantener y mejorar su estado de salud actual, previniendo, en la medida de lo posible, las enfermedades crónicas frecuentes de la edad adulta.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon9.png",
+    title: "Perfil Cardiovascular",
+    text: "La detección de posibles alteraciones bioquímicas relacionadas con determinadas patologías, a través de una muestra de sangre y de orina. El chequeo incluye un hemograma completo y parámetros bioquímicos claves para la realización de una revisión general.",
+  },
+  {
+    component: "IconRow",
+    imageSrc: "/src/assets/genetica/genetica-icon5.png",
+    title: "Rendimiento Deportivo",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon10.png",
+    title: "Chequeo deportivo avanzado",
+    text: "Evalúa diferentes parámetros analíticos claves para un correcto estado de salud y un óptimo rendimiento deportivo, permitiéndote conocer cuáles son susceptibles de mejora.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon11.png",
+    title: "Test Lesiones",
+    text: "Información acerca de la susceptibilidad a sufrir lesiones durante el entrenamiento, así como la respuesta en función del tipo de ejercicio de forma individualizada.",
+  },
+  {
+    component: "IconRow",
+    imageSrc: "/src/assets/genetica/genetica-icon6.png",
+    title: "Prevención de Cancer",
+  },
+  {
+    component: "ImageRow",
+    imageSrc: "/src/assets/genetica/genetica2.png",
+    html: `
+    <h3>Tipología de Test en Oncología:</h3>
+    <p>
+      <strong>Colón.</strong> Detección precoz del cáncer de colon mediante un
+      análisis de sangre.
+      <br />
+      <strong>Biopsia líquida</strong> mediante secuenciación para tumores
+      sólidos en estadios avanzados (73 genes).
+    </p>
+    <ul class="plan-vida-p">
+      <li>
+        <span>
+          Análisis dirigido de mutaciones tumorales en sangre (3 genes).
+        </span>
+      </li>
+      <li>
+        <span>Detección del cáncer de próstata.</span>
+      </li>
+    </ul>
+    <p>
+      <strong>Test Onco Farmacogenética</strong> del tratamiento con
+    </p>
+    <ul class="plan-vida-p">
+      <li>
+        <span>5 fluorouracilo (capecitabina).</span>
+      </li>
+      <li>
+        <span>Irinotecan.</span>
+      </li>
+      <li>
+        <span>Lapatinib.</span>
+      </li>
+      <li>
+        <span>Metrotrexato.</span>
+      </li>
+      <li>
+        <span>Tamoxifeno.</span>
+      </li>
+      <li>
+        <span>Tiopurinas.</span>
+      </li>
+      <li>
+        <span>Cisplatino.</span>
+      </li>
+    </ul>`,
+  },
+  {
+    component: "IconRow",
+    imageSrc: "/src/assets/genetica/genetica-icon7.png",
+    title: "Futuros Padres",
+  },
+  {
+    component: "ImageRow",
+    imageSrc: "/src/assets/genetica/genetica3.png",
+    html: `
+    <h3>Test Prenatal</h3>
+    <p>
+      Analiza el ADN libre fetal presente en la sangre materna para
+      determinar el riesgo de que el feto sea portador de las anomalías
+      cromosómicas estudiadas. La prueba está indicada en embarazadas con
+      al menos 10 semanas de gestación, en las siguientes situaciones:
+    </p>
+    <ul class="plan-vida-p">
+      <li><span>Embarazo único o gemelar (2 fetos).</span></li>
+      <li><span>Embarazos por FIV.</span></li>
+      <li><span>Casos de ovodonación.</span></li>
+      <li>
+        <span>La prueba debe ser prescrita por un especialista.</span>
+      </li>
+    </ul>`,
+  },
+  {
+    component: "ImageRow",
+    imageSrc: "/src/assets/genetica/genetica4.png",
+    html: `
+    <h3>Test Prenatal</h3>
+    <h3>Test Genético</h3>
+    <p>
+      Analiza tu ADN, determinando si eres portador o no de alguna alteración en
+      tus genes. El test analiza 176 enfermedades monogénicas seleccionadas por
+      su gravedad y prevalencia, excluyendo las enfermedades sin relevancia
+      clínica incluidas en otras pruebas. Si ambos progenitores sois portadores
+      de la misma enfermedad, se debe acudir a un especialista para recibir un
+      asesoramiento genético apropiado.
+    </p>`,
+  },
+  {
+    component: "IconRow",
+    imageSrc: "/src/assets/genetica/genetica-icon8.png",
+    title: "Otros",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon12.png",
+    title: "Farmacogenética",
+    text: "Análisis de toxicidad de los fármacos de uso habitual para determinar la elección más adecuada.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon13.png",
+    title: "Glaucoma PEX test",
+    text: "Riesgo genético de padecer glaucoma pseudoexfoliativo.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon14.png",
+    title: "Macula Test",
+    text: "Riesgo genético de padecer degeneración macular asociada a la edad.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon15.png",
+    title: "Metales en el Organismo",
+    text: "Análisis en cabello de la presencia de metales para evaluar una posible toxicidad en tu dieta o en tu entorno.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon16.png",
+    title: "Huella Genética",
+    text: "Conserva tu DNI genético para el futuro.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon17.png",
+    title: "Test de Paternidad",
+    text: "Prueba de ADN para determinar la paternidad biológica.",
+  },
+  {
+    component: "IconColumn",
+    imageSrc: "/src/assets/genetica/genetica-img-icon7.png",
+    title: "Test Linfocitario",
+    text: "Evaluación del sistema inmunitario.",
+  },
+];
