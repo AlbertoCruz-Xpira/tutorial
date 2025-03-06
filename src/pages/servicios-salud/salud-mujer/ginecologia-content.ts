15c770a613073fcc69b17ffa84888a9270791a52
Alberto Cruz <xpiradesign@gmail.com>
2025-03-06 14:36:15 +0100

    actualización de renderización de cuerpos dinamicos de paginas

---
+++ src/pages/servicios-salud/salud-mujer/ginecologia-content.ts
@@ -0,0 +1,299 @@
+export interface BaseContent {
+  icon: string;
+  title: string;
+  imageSrc: string;
+  imageDesc: string;
+  description_1: string;
+  orientation: number;
+  description_2: string;
+}
+
+export const content: BaseContent[] = [
+  {
+    icon: "/src/assets/salud-mujer/ginecologia/icon-proposito-ginecologia.png",
+    title: "El propósito de estas visitas",
+    imageSrc: "/src/assets/salud-mujer/ginecologia/proposito-ginecologia.png",
+    imageDesc: "",
+    description_1: `
+      <ul>
+        <li>
+          <span>Prevención de enfermedades.</span>
+        </li>
+        <li>
+          <span>Detectar riesgo de problemas médicos futuros.</span>
+        </li>
+        <li>
+          <span>Orientar sobre estilos y calidad de vida.</span>
+        </li>
+        <li>
+          <span>Revisión y recomendación de las vacunas correspondientes.</span>
+        </li>
+        <li>
+          <span>Seguimiento y control por un médico de forma directa.</span>
+        </li>
+      </ul>
+    `,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/salud-mujer/ginecologia/icon-evaluacion-ginecologia.png",
+    title: "Evaluación ginecológica anual",
+    imageSrc: "/src/assets/salud-mujer/ginecologia/evaluacion-ginecologia.png",
+    imageDesc: "",
+    description_1: `
+      <ul>
+        <li>
+          <span>Consulta inicial (exploración y pruebas).</span>
+        </li>
+        <li>
+          <span>Citología.</span>
+        </li>
+        <li>
+          <span>Ecografía abdominal o transvaginal.</span>
+        </li>
+        <li>
+          <span>Consulta de Reevaluación.</span>
+        </li>
+      </ul>
+    `,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/salud-mujer/ginecologia/icon-prevencion-ginecologia.png",
+    title: "Valoración y prevención",
+    imageSrc: "/src/assets/salud-mujer/ginecologia/valoracion-ginecologia.png",
+    imageDesc: `<p>
+        Nota: Si padece cardiopatía, problemas renales o algunas otras
+        afecciones, es posible que necesite un control más estricto.
+      </p> `,
+    description_1: `
+    <div>
+        <ul>
+          <li><span>Orientación sobre hábitos de vida saludable.</span></li>
+          <li><span>Control de Talla Peso e IMC.</span></li>
+          <li><span>Medición de la presión arterial.</span></li>
+          <li>
+            <span
+              >Control analítico que incluye Hemograma y bioquímica completa.</span
+            >
+          </li>
+          <li><span>Examen de colesterol.</span></li>
+          <li><span>Examen para diabetes.</span></li>
+          <li><span>Pruebas de función tiroidea.</span></li>
+          <li><span>Radiografía de tórax PA y Lateral.</span></li>
+          <li><span>Ecografía abdominal.</span></li>
+          <li><span>Audiometría tonal bilateral.</span></li>
+          <li><span>Espirometría.</span></li>
+          <li>
+            <span
+              >Revisión por cardiólogo: electrocardiograma y prueba de esfuerzo.</span
+            >
+          </li>
+          <li>
+            <span
+              >Revisión oftalmológica por optometrista (óptica): tonometría,
+              agudeza visual y exploración del fondo de ojo.</span
+            >
+          </li>
+          <li>
+            <span
+              >Revisión ginecológica: consulta con un especialista del
+              Departamento de Ginecología y Obstetricia, citología vaginal y
+              ecografía ginecológica. Mamografía y ecografía mamaria a criterio
+              de ginecólogo. También disponer de estudios genéticos con alta
+              sensibilidad diagnostica.</span
+            >
+          </li>
+          <li>
+            <span
+              >Visite al odontólogo cada año para un examen y limpieza dentales.</span
+            >
+          </li>
+        </ul>
+      </div>
+    `,
+    orientation: 0,
+    description_2: `
+      <div class="pb-5">
+        <p>
+          Las valoraciones ginecológicas periódicas nos ayudan a la detección
+          temprana de posibles problemas de salud, que con un diagnóstico precoz
+          lograremos una resolución satisfactoria. Es importante mantener unos
+          hábitos de vida saludable, para controlar la tensión arterial,
+          colesterol y azúcar en sangre.
+        </p>
+      </div>`,
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/ginecologia/valoracion-ginecologia-2.png",
+    imageDesc: "",
+    description_1: `
+      <h3>Vacunas</h3>
+      <ul>
+        <li><span>Vacuna antigripal anual.</span></li>
+        <li><span>Vacuna contra el tétanos, difteria y tos ferina.</span></li>
+        <li><span>Vacuna contra el VPH (18-26 años).</span></li>
+        <li>
+          <span>Vacuna contra la varicela si no ha pasado la enfermedad.</span
+          >
+        </li>
+        <li><span>Otras vacunas según riesgos específicos.</span></li>
+      </ul>
+      <p>
+        Cada mujer debe realizarse un autoexamen de mamas. El médico debe
+        hacer un examen completo cada tres años para mujeres de 20 a 40 años.
+      </p>
+    `,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/ginecologia/valoracion-ginecologia-3.png",
+    imageDesc: "",
+    description_1: `
+      <h3>Examen pélvico y citología vaginal</h3>
+        <p>
+          A partir de los 21 años, o también antes si se han mantenido ya
+          relaciones sexuales, y repetirla al menos cada 3 años hasta los 65 si
+          se han obteniendo resultados normales en las últimas valoraciones,
+          para vigilar los primeros signos de patología de cuello uterino. En
+          mujeres sexualmente activas se realizan pruebas de detección para
+          infección por clamidia y/o ITS.
+        </p>
+    `,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/ginecologia/valoracion-ginecologia-4.png",
+    imageDesc: "",
+    description_1: `
+      <h3>Ecografía Mamaria</h3>
+        <p>
+          Es útil para distinguir estructuras líquidas de sólidas y por lo tanto
+          a menudo se usa como recurso para detectar quistes (estructuras
+          pequeñas rellenas de líquido) dentro de la mama. Y nos ayuda a
+          diferencias entre masas sólidas y decidir necesidad de biopsia.
+        </p>
+        <h3>Mamografía</h3>
+        <p>Depende de la clínica puede utilizarse como método de:</p>
+        <ul>
+          <li>
+            <span
+              >Mamografías de screening o cribado: No existe clínica y de
+              realiza para buscar signos precoces. Se comienza a realizar a
+              partir de los 40 años se deben practicar una mamografía cada 1 a 2
+              años, dependiendo de los factores de riesgo.</span
+            >
+          </li>
+          <li>
+            <span
+              >Mamografía diagnostica: Se realiza si hay sospecha o signos
+              clínicos sugestivos, o un resultado indeterminado de la prueba de
+              screening.</span
+            >
+          </li>
+        </ul>
+    `,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/ginecologia/valoracion-ginecologia-5.png",
+    imageDesc: "",
+    description_1: `
+      <h3>Ecografía ginecológica</h3>
+      <p>
+          Tiene como objetivo visualizar los órganos genitales femeninos.
+          Según la vía por la que se realice la ecografía, podemos hablar de:
+      </p><br>
+      <ul class="plan-vida-p">
+          <li><span>Ecografía transabdominal: se puede visualizar un área mayor, pero tiene peor definición.</span></li>
+          <li><span>Ecografía transvaginal: tiene una óptima definición, el ecógrafo se sitúa muy próximo al objeto de estudio, tiene una gran sensibilidad. Pero el área a estudiar es menor.</span></li>
+      </ul>
+      <p><strong>Indicaciones</strong></p><br>
+      <ul class="plan-vida-p">
+          <li><span>Patología tumoral benigna y maligna en cérvix, endometrio, miometrio, trompas y ovarios.</span></li>
+          <li><span>Esterilidad: análisis de posibles lesiones que justifiquen esta situación.</span></li>
+          <li><span>Reproducción.</span></li>
+          <li><span>Uroginecología.</span></li>
+          <li><span>Para guiar procedimientos invasivos.</span></li>
+      </ul>
+    `,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `
+      <div class="col-md-12 margintop35">
+        <h3>Métodos anticonceptivos</h3>
+        <p>Son los que previenen un embarazo. Pueden ser hormonales o no hormonales, temporales o definitivos, los cuales deben de adecuarse a cada persona según necesidades.</p><br>
+        <h4 class="h4color">Métodos hormonales y Químicos</h4>
+        <p>
+            Los métodos hormonales y químicos son medicamentos anticonceptivos que impiden que el proceso de concepción pueda tener lugar. La anticoncepción hormonal se puede aplicar de diversas formas:
+        </p><br>
+        <ul class="metodos-anti">
+            <li class="metodo1"><strong>Anillo vaginal:</strong> único de administración vaginal mensual. Es un anillo transparente, suave y flexible que se coloca por la misma usuaria por vía vaginal, liberando diariamente dosis bajas de hormonas.</li>
+            <li class="metodo2"><strong>Píldora anticonceptiva:</strong> son pastillas que contienen hormonas similares a las que producen los ovarios (estrógenos y gestágenos). Funcionan impidiendo la ovulación y por tanto el embarazo. Son de administración diaria por vía oral</li>
+            <li class="metodo3"><strong>Parches anticonceptivos:</strong> es una pequeña lámina adhesiva de plástico que contiene las hormonas que impiden la ovulación y por tanto el embarazo. Esta lámina se pega a la piel y las hormonas se van liberando lentamente, pasando al organismo a través de la piel.</li>
+            <li class="metodo4"><strong>Anticonceptivo subdérmico:</strong> implante hormonal compuesto por una varilla del tamaño de una cerilla que se coloca bajo la piel del brazo de la mujer, ofreciendo protección anticonceptiva entre 3 y 5 años. Sin embargo, el médico puede retirarlo en cualquier momento y la mujer recuperará la fertilidad en un tiempo mínimo.</li>
+            <li class="metodo5"><strong>Píldora sin estrógenos o píldora 0 estrógenos o minipíldora:</strong> píldora libre de estrógenos, recomendada para mujeres que no pueden o no desean tomarlos; la dosis hormonal es tan ligera que entre otras indicaciones es la única píldora recetada durante la lactancia.</li>
+            <li class="metodo6"><strong>Píldora trifásica:</strong> método anticonceptivo altamente eficaz de dosis hormonales bajas con un balance hormonal suave y escalonado; imita el ciclo fisiológico de la mujer en forma secuencial progresiva, brindando estricto control del ciclo. Además, reduce la grasa facial, por lo que puede ser indicado para el tratamiento de acné leve a moderado.</li>
+            <li class="metodo7"><strong>Método hormonal inyectable o inyección intramuscular anticonceptiva:</strong> método de larga duración que se inyecta en un músculo. Se debe aplicar en el periodo adecuado, en caso contrario es necesario usar métodos de barrera para evitar el embarazo.</li>
+        </ul>
+        <p>
+            Es un pequeño aparato de plástico que tiene normalmente forma de T con un hilo colgante en su extremo más largo, y que se introduce dentro del útero para impedir el paso de los espermatozoides y evitar que el óvulo se deposite en la pared del útero.
+            El DIU sólo lo puede colocar y retirar un ginecólogo. Una vez colocado su eficacia anticonceptiva es de 3 a 5 años aproximadamente.
+        </p><br>
+        <h4 class="h4color">
+            Ligadura de trompas: Generalmente permanente y no reversible.
+        </h4>
+        <p>
+            Consiste en ligar las trompas de Falopio impidiendo así el paso de los óvulos desde los ovarios hasta el útero y de esta forma evitar el embarazo. Se realiza mediante una pequeña intervención quirúrgica bajo anestesia. Una vez realizada se sigue manteniendo la menstruación y la ovulación, y no se ve afectada ni la capacidad ni el deseo sexual.
+        </p>
+    </div>
+    `,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/ginecologia/valoracion-ginecologia-6.png",
+    imageDesc: "",
+    description_1: `
+      <h3>Examen para cáncer de colon </h3>
+      <p>
+          Edades entre 50 y 75 años deben ser examinados en búsqueda de cáncer colorrectal:
+      </p><br>
+      <ul class="plan-vida-p">
+          <li><span>Examen coprológico anual (Sangre Oculta - Inmunoquímica Fecal)</span></li>
+          <li><span>Sigmoidoscopia flexible cada 5 años. </span></li>
+          <li><span>Colonoscopia cada 10 años.</span></li>
+          <li><span>Colonografía tomográfica computarizada (colonoscopia virtual) cada 5 años.</span></li>
+      </ul>
+      <p>Si tiene factores de riesgo para el cáncer de colon (colitis ulcerativa prolongada, cáncer colorrectal o antecedentes de adenomas colorrectales grandes) pueden necesitar una colonoscopia con más frecuencia.</p><br><br>
+    `,
+    orientation: 0,
+    description_2: "",
+  },
+];
