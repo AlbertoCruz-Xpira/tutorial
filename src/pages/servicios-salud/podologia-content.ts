71940a6a09ef35009aee77b0eed3b3af1976add7
Alberto Cruz <xpiradesign@gmail.com>
2025-03-07 15:29:11 +0100

    Podología

---
+++ src/pages/servicios-salud/podologia-content.ts
@@ -0,0 +1,152 @@
+export interface BaseContent {
+  icon: string;
+  title: string;
+  imageSrc: string;
+  imageDesc: string;
+  description_1: string;
+  orientation: number;
+  description_2: string;
+}
+
+export const content: BaseContent[] = [
+  {
+    icon: "/src/assets/podologia/podologia-icon1.png",
+    title: "Quiropodia",
+    imageSrc: "/src/assets/podologia/podologia1.png",
+    imageDesc: "",
+    description_1: `<p>
+              Prevenir, diagnosticar y tratar cualquier problema relacionado con
+              el pie y las uñas. Un mal apoyo de los pies, un calzado
+              inadecuado, infecciones víricas fúngicas o bacterianas pueden
+              derivar en una lesión en el pie que nos limita nuestra calidad de
+              vida.
+            </p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/podologia/podologia-icon2.png",
+    title: "Hiperqueratosis",
+    imageSrc: "/src/assets/podologia/podologia2.png",
+    imageDesc: "",
+    description_1: `<p>
+              Es la presión o roce mantenido intermitente produciendo un
+              desplazamiento de las capas de la piel. Puede estar relacionado
+              con una presión externa como puede ser el zapato o una presión
+              interna por ejemplo un dedo deformado. Este engrosamiento de la
+              piel puede producir molestias y puede ir acompañado de otras
+              lesiones de mayor dureza.
+              <br />
+              Tipos:
+            </p>
+            <ul>
+              <li>
+                <span> Los helomas o callos. </span>
+              </li>
+              <li>
+                <span> Papilomas (Verrugas). </span>
+              </li>
+            </ul>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "/src/assets/podologia/podologia3.png",
+    imageDesc: "",
+    description_1: `
+          <h3>Prevención pie diabético</h3>
+          <p>
+            El factor traumático es el desencadenante de una uña encarnada. La
+            presión directa sobre la uña bien desde el dorso o bien desde los
+            laterales produce que se clave la uña en la piel de los bordes del
+            dedo.
+          </p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "/src/assets/podologia/podologia4.png",
+    imageDesc: "",
+    description_1: `<h3>Problemas de uñas</h3>
+                <p>El tratamiento de la quiropodia se basa en el corte y fresado de las uñas. En el caso que el podólogo considere recomendará también el uso de medicamentos para tratar la posible infección. A su vez, en caso de sospecha de posible infección por hongos, el podólogo obtendrá una muestra de la uña para realizar un cultivo de la uña y poder aislar el microorganismo que esté causando el problema infeccioso y tratarlo correctamente.</p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/podologia/podologia-icon3.png",
+    title: "Cirugías",
+    imageSrc: "/src/assets/podologia/podologia5.png",
+    imageDesc: "",
+    description_1: `<p>Existen algunos problemas de los pies que se pueden resolver de manera definitiva mediante cirugía.</p>
+                <h3 class="margintop15">Prevención pie diabético</h3>
+                <p>El factor traumático es el desencadenante de una uña encarnada. La presión directa sobre la uña bien desde el dorso o bien desde los laterales produce que se clave la uña en la piel de los bordes del dedo.</p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/podologia/podologia-icon5.png",
+    title: "Podología deportiva",
+    imageSrc: "/src/assets/podologia/podologia6.png",
+    imageDesc: "",
+    description_1: `<h3>Juanetes</h3>
+      <p>
+        El origen de los juanetes en mayor parte de los casos suele producirse
+        por cambios biomecánicos del pie. También existe un componente genético
+        hereditario que predisponen a que el pie de deforme hacia los juanetes,
+        los pacientes heredan la forma de los dedos y de los pies en general. Se
+        usan medidas conservadoras como plantillas o cambios de calzado cuya
+        finalidad es mejorar los síntomas, pero no corrigen la deformidad. La
+        única solución definitiva es la cirugía.
+      </p>
+      <h3>Metatarsalgia</h3>
+      <p>
+        Es el dolor, hinchazón deformidad, cojera, alteraciones de la piel como
+        hiperqueratosis o helomas y dificultad para ponerse zapatos.
+      </p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/podologia/podologia-icon4.png",
+    title: "Curación de heridas",
+    imageSrc: "/src/assets/podologia/podologia7.png",
+    imageDesc: "",
+    description_1: `<p>Se aplican los protocolos de tratamiento con los que se logran reducir el porcentaje de aparición de complicaciones en los pies y piernas. La mala circulación venosa en las piernas es, a menudo, la causa de distintos problemas en los miembros inferiores tales como piernas hinchadas o doloridas, varices e incluso úlceras.</p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/podologia/podologia-icon5.png",
+    title: "Podología deportiva",
+    imageSrc: "/src/assets/podologia/podologia8.png",
+    imageDesc: "",
+    description_1: `<p>El tratamiento conjunto y coordinado de nuestros podólogos y fisioterapeutas es esencial para la correcta resolución de la lesión deportiva. El conjunto de análisis biomecánico de la marcha y gesto deportivo en coordinación con la valoración y diagnóstico fisioterapéutico nos llevará a la pauta del plan de tratamiento y de recuperación y readaptación a la práctica deportiva del paciente que tenga una lesión. A su vez, una vez resuelta o en el caso de no tener lesión, el tratamiento irá enfocado a la prevención de esta haciendo un estudio y viendo de manera precoz cuales pueden ser los factores de riesgo para desarrollar una lesión.</p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/podologia/podologia-icon6.png",
+    title: "Podología personas mayores",
+    imageSrc: "/src/assets/podologia/podologia9.png",
+    imageDesc: "",
+    description_1: `<p>
+            A lo largo de nuestras vidas, nuestros pies son los encargados de
+            transportarnos. Los pies han soportado muchos pasos y el peso del
+            cuerpo. Es por ello por lo que la edad produce una serie de cambios
+            en la piel y en las uñas que se tiene que cuidar.
+          </p>
+          <br />
+          <p>
+            La grasa de la planta del pie puede modificar su propiedad de
+            amortiguación y a su vez tener también deshidratación lo que provoca
+            que se produzcan más lesiones. Las uñas también sufren cambios en su
+            estructura y en su grosor.
+          </p>`,
+    orientation: 0,
+    description_2: "",
+  },
+];
