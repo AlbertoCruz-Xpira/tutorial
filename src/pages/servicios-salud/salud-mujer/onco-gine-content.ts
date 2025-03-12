680b665d9b2e070eb7a2d1381b2403e08404835e
Alberto Cruz <xpiradesign@gmail.com>
2025-03-12 17:50:30 +0100

    evolución salud mujer

---
+++ src/pages/servicios-salud/salud-mujer/onco-gine-content.ts
@@ -0,0 +1,327 @@
+import type { BaseContent } from "../../../interface/BaseContent";
+
+export const content: BaseContent[] = [
+  {
+    icon: "/src/assets/salud-mujer/onco-gine/icon-oncogine1.png",
+    title: "Nuestras recomendaciones preventivas para ti",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "/src/assets/salud-mujer/onco-gine/img-oncogine1.png",
+    imageDesc: "",
+    description_1: `<ul class="plan-vida-p">
+                    <li><span><strong>Exploración Ginecológica:</strong> Se buscarán signos y síntomas en genitales externos y tacto vaginal, permitiendo la valoración de útero y ovarios.</span></li>
+                    <li><span>Mamografías.</span></li>
+                    <li><span>Eco Abdomino-Pélvica.</span></li>
+                    <li><span><strong>Eco Transvaginal:</strong> Visualizamos útero, trompas y ovarios buscando masas sugestivas.</span></li>
+                    <li><span><strong>Histeroscopia Diagnóstica:</strong>  Si se confirma sospecha de tumor se introduce una cámara a través del cuello del útero, esta prueba es sencilla y se realiza de forma ambulatoria.</span></li>
+                    <li><span><strong>Citología:</strong> Analizamos células del cérvix para detectar presencia de infecciones, lesiones precancerosas y cáncer.</span></li>
+                    <li><span><strong>Colposcopia:</strong> Se realiza en la misma consulta y visualizamos el útero, vagina y vulva.</span></li>
+                    <li><span><strong>Análisis de sangre:</strong> Nos da información adyuvante, dando parámetros que puedan ser sugestivos de presencia de cáncer.</span></li>
+                    <li><span><strong>Biopsia:</strong> Con sospecha positiva es necesario extraer muestras para analizarlas.</span></li>
+                    <li><span>Realización el ganglio centinela.</span></li>
+                </ul>`,
+    orientation: 0,
+    description_2: `
+      <p>Realiza un tratamiento integral del cáncer en la mujer, desde su diagnóstico hasta su curación, lo que implica la puesta en marcha de un programa de detección precoz (chequeos), tratamiento de la enfermedad primaria y recidivante.</p>
+    
+      <p>
+          Los tipos de tumores que se tratan son: tumores de mama, ovario,
+          trompas de Falopio, cuello uterino, endometrio, miometrio, vulva,
+          vagina, y peritoneo.
+        </p>`,
+  },
+  {
+    icon: "/src/assets/salud-mujer/onco-gine/icon-oncogine2.png",
+    title: "Valoración en equipo multidisciplinar:",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "/src/assets/salud-mujer/onco-gine/img-oncogine2.png",
+    imageDesc: "",
+    description_1: `<p>Realiza un tratamiento integral del cáncer en la mujer, desde su diagnóstico hasta su curación, lo que implica la puesta en marcha de un programa de detección precoz (chequeos), tratamiento de la enfermedad primaria y recidivante.</p><br>
+<p>Los tipos de tumores que se tratan son: tumores de mama, ovario, trompas de Falopio, cuello uterino, endometrio, miometrio, vulva, vagina, y peritoneo.</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/salud-mujer/onco-gine/icon-oncogine3.png",
+    title: "Tratamiento",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "/src/assets/salud-mujer/onco-gine/img-oncogine3.png",
+    imageDesc: "",
+    description_1: `<ul class="plan-vida-p">
+                    <li><span>Cirugía radical del cáncer ginecológico.</span></li>
+                    <li><span>Cirugía mini-invasiva laparoscópica o robótica del cáncer ginecológico.</span></li>
+                    <li><span>Conservación de la fertilidad en mujeres con cáncer ginecológico.</span></li>
+                    <li><span>Quimioterapia.</span></li>
+                    <li><span>Radioterapia.</span></li>
+                    <li><span>Reconstrucción estética de la mama.</span></li>
+                </ul>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/salud-mujer/onco-gine/icon-oncogine4.png",
+    title: "Otros",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "/src/assets/salud-mujer/onco-gine/img-oncogine4.png",
+    imageDesc: "",
+    description_1: `<h3>Cáncer de piel:</h3>
+                <p>Cualquier persona que pasa tiempo bajo el sol puede padecer cáncer de piel. Las personas de piel blanca, especialmente las que tienen el cabello rubio o que son pelirrojas, tienen una mayor probabilidad de padecer este cáncer que las personas de piel más oscura. Las personas que hayan tenido un familiar cercano con melanoma y aquellas que han tenido quemaduras solares graves durante la niñez tienen una mayor probabilidad de padecer cáncer de piel.</p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<p>
+  La mayoría de los cánceres de piel pueden prevenirse limitando la exposición a
+  rayos ultravioleta (UV) provenientes del sol y de otras fuentes como las camas
+  de bronceado.
+</p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "Examen de la piel:",
+    imageSrc: "/src/assets/salud-mujer/onco-gine/img-oncogine5.png",
+    imageDesc: "",
+    description_1: `<p class="p-eco p-pink">
+                    <span>Examen de la piel:</span><br>
+                    Prestar atención a todos los lunares y las manchas de su piel.
+                </p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/salud-mujer/onco-gine/img-oncogine6.png",
+    title: "Biopsia de piel",
+    imageSrc: "/src/assets/salud-mujer/onco-gine/img-oncogine6.png",
+    imageDesc: "",
+    description_1: `<p class="p-eco p-pink">
+                    <span>Biopsia de piel: </span><br>
+                    Se puede tomar una muestra de la piel sospechosa para realizar un análisis para diagnostico y saber el abordaje terapéutico.
+                </p>`,
+    orientation: 0,
+    description_2: "",
+  },
+
+  {
+    icon: "",
+    title: "",
+    imageSrc: "/src/assets/salud-mujer/onco-gine/img-oncogine2.png",
+    imageDesc: "",
+    description_1: `<h3>Cáncer de Pulmón</h3>
+                <p>Al menos 8 de cada 10 pacientes diagnosticados, se deben al hábito de fumar. Sin embargo, las personas que no fuman pueden también pueden padecerlo.</p>
+                <p>Es uno de los pocos cánceres que a menudo se puede prevenir: simplemente no fume.</p>
+                <p>Si fuma o es un exfumador, tiene de 55 a 74 años, y está relativamente bien de salud, usted podría beneficiarse de pruebas de imagen como Rx y/o tomografía computarizada realizada cada año como prueba de detección del cáncer de pulmón.</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "/src/assets/salud-mujer/onco-gine/img-oncogine7.png",
+    imageDesc: "",
+    description_1: `<div class="col-md-6">
+                <h3>Cáncer de Colon:</h3>
+                <p>Si existe un antecedente personal o familiar de este cáncer, o que tengan pólipos en el colon o el recto, o aquellas con enfermedad inflamatoria intestinal, son más propensas a padecer cáncer de colon. Además, el sobrepeso, la alimentación basada principalmente en alimentos con un alto contenido de grasa (especialmente de fuentes animales), fumar y la falta de ejercicio puede causar que una persona tenga más probabilidad de padecer este cáncer. Las pruebas pueden salvar vidas porque encuentran los pólipos antes de que se vuelvan cancerosos. Si se extirpan los pólipos precancerosos, se puede prevenir el cáncer de colon. El cribado de Ca Colorrectal se debe comenzar a los 45 años:</p><br>
+                <ul class="plan-vida-p">
+                    <li><span>Prueba inmunoquímica fecal altamente sensible (FIT) cada año.</span></li>
+                    <li>
+                        <span>
+                            Prueba de sangre oculta en heces fecales basada en guayacol (gFOBT)
+                            altamente sensible, cada año.
+                        </span>
+                    </li>
+                    <li><span>Prueba multidirigida de ADN en heces fecales cada 3 años.</span></li>
+                    <li><span>Colonoscopia cada 10 años.</span></li>
+                    <li><span>Colonografía CT (colonoscopia virtual) cada 5 años.</span></li>
+                    <li><span>Sigmoidoscopia flexible cada 5 años.</span></li>
+                </ul>
+            </div>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "/src/assets/salud-mujer/onco-gine/icon-oncogine4.png",
+    title: "Pruebas diagnósticas",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "/src/assets/salud-mujer/onco-gine/img-oncogine8.png",
+    imageDesc: "",
+    description_1: `<p>
+                    Se han reseñado más de 50 síndromes hereditarios de cáncer. La mayoría de ellos son causados por mutaciones altamente penetrantes que se heredan en forma dominante. La lista a continuación incluye algunos de los síndromes hereditarios de cáncer más comunes para los que se dispone de pruebas genéticas; incluye también el gen o genes mutados en cada síndrome, así como los tipos de cáncer asociados con más frecuencia con estos síndromes.
+                </p>`,
+    orientation: 1,
+    description_2: ``,
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>Síndrome hereditario de cáncer de mama y de cáncer de ovario</h4>
+                    <p><strong>Genes: BRCA1, BRCA2</strong></p>
+                    <p>Tipos relacionados de cáncer: cáncer de mama en mujeres, cáncer de ovarios y otros cánceres, incluso de próstata, de páncreas y de seno en hombres</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>Síndrome de Li-Fraumeni</h4>
+                    <p><strong>Gen: TP53</strong></p>
+                    <p>Tipos relacionados de cáncer: cáncer de mama, sarcoma de tejido blando, osteosarcoma (cáncer de hueso), leucemia, tumores cerebrales, carcinoma corticosuprarrenal (cáncer de glándulas suprarrenales) y otros cánceres</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>Síndrome de Cowden (síndrome de hamartoma PTEN)</h4>
+                    <p><strong>Gen: PTEN</strong></p>
+                    <p>Tipos relacionados de cáncer: mama, tiroides, endometrio (revestimiento del útero), y otros cánceres</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>Síndrome de Lynch (cáncer colorrectal hereditario sin poliposis)</h4>
+                    <p><strong>Genes: MSH2, MLH1, MSH6, PMS2, EPCAM</strong></p>
+                    <p>Tipos relacionados de cáncer: cáncer de colon y recto, de endometrio, ovarios, pelvis renal, páncreas, intestino delgado, hígado y vías biliares, de estómago, de cerebro y de seno</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>Poliposis adenomatosa familiar</h4>
+                    <p><strong>Gen: APC</strong></p>
+                    <p>Tipos relacionados de cáncer: cáncer colorrectal, pólipos múltiples no cancerosos de colon, y tumores tanto benignos (no cancerosos) como cancerosos de intestino delgado, de cerebro, estómago, hueso, de piel y de otros tejidos</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>Retinoblastoma</h4>
+                    <p><strong>Gen: RB1</strong></p>
+                    <p>Tipos relacionados de cáncer: cáncer de ojo (cáncer de retina), pinealoma (cáncer de glándula pineal), osteosarcoma, melanoma y sarcoma de tejido blando</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>Neoplasia endocrina múltiple tipo 1 (síndrome de Wermer):</h4>
+                    <p><strong>Gen: MEN1</strong></p>
+                    <p>Tipos relacionados de cáncer: tumores endocrinos de páncreas y tumores (generalmente benignos) de glándula paratiroides y de glándula hipófisis</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>Neoplasia endocrina múltiple tipo 2:</h4>
+                    <p><strong>Gen: RET</strong></p>
+                    <p>Tipos relacionados de cáncer: cáncer medular de tiroides y feocromocitoma (tumor benigno de glándulas suprarrenales)</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>Síndrome de Von Hippel-Lindau:</h4>
+                    <p><strong>Gen: VHL</strong></p>
+                    <p>Tipos relacionados de cáncer: cáncer de riñón y muchos tumores no cancerosos, como el feocromocitoma</p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h3>Citogenética/FISH: </h3>
+                <p>Detección y confirmación de alteraciones génicas y cromosómicas (amplificaciones, traslocaciones o deleciones) únicas o en función del tipo de cáncer por paneles específicos:</p>
+                <ul class="plan-vida-p">
+                    <li><span>AML FISH Panel: D8Z2, AML/ETO, MLL, PML/RARA.</span></li>
+                    <li><span>CBFBNHL FISH Panel: ALK, BCL6, ATM, IGH, p53.</span></li>
+                </ul>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h3>Test Moleculares:</h3>
+                <p>Detección de mutaciones somáticas únicas en genes como BRCA, cKIT, AKT, EGFR, KRAS, PI3CA, etc, o de paneles genéticos específicos de cáncer:</p>
+                <ul class="plan-vida-p">
+                    <li><span>AML Panel: FLT3 / NPM1Anti-EGFR.</span></li>
+                    <li><span>Therapy Panel: EGFR/KRAS/BRAF/PI3CA/ALKT para el diagnóstico y pronóstico tumoral y la asociación a tratamientos oncológicos.</span></li>
+                    <li><span>Pruebas para la la confirmación y detección de genes únicos en pacientes con historial de cáncer hereditario.</span></li>
+                </ul>`,
+    orientation: 1,
+    description_2: "",
+  },
+];
---
+++ src/pages/servicios-salud/salud-mujer/reproduccion-asistida-content.ts
@@ -0,0 +1,193 @@
+import type { BaseContent } from "../../../interface/BaseContent";
+
+export const content: BaseContent[] = [
+  {
+    icon: "/src/assets/salud-mujer/reproduccion-asistida/icon-reproduccion.png",
+    title: "Inseminación Artificial (IA)",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/reproduccion-asistida/reproduccion-1.png",
+    imageDesc: "",
+    description_1: `<ul>
+            <li>
+              <span
+                >Nos permite observar la localización, tamaño y frecuencia
+                cardiaca del embrión. Se realiza entre la semana 6 y 9 de
+                embarazo.</span
+              >
+            </li>
+            <li>
+              <span
+                ><strong>Cribaje de primer trimestre:</strong> Descarta alteraciones
+                de los cromosomas, como por ejemplo el Síndrome de Down. Consta de
+                una analítica de sangre (semana 8-10) y una ecografía detallada (semana
+                11-13)</span
+              >
+            </li>
+            <li>
+              <span
+                ><strong>Ecografía semana 20-22:</strong> Durante la misma se observa
+                la morfología del feto.</span
+              >
+            </li>
+          </ul>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/reproduccion-asistida/reproduccion-2.png",
+    imageDesc: "",
+    description_1: `<h4>¿Qué es la Inseminación Artificial?</h4>
+          <p>
+            La Inseminación Artificial es una de las técnicas usadas por la
+            Medicina Reproductiva cuando existe un problema de fertilidad y que
+            consiste en la colocación de una muestra de semen, previamente
+            preparada en el laboratorio, en el interior del útero de la mujer
+            con el fin de incrementar el potencial de los espermatozoides y las
+            posibilidades de fecundación del óvulo. De esta manera, se acorta la
+            distancia que separa al espermatozoide del óvulo y facilitamos el
+            encuentro entre ambos, aumentando las posibilidades de conseguir el
+            tan deseado embarazo. A diferencia de la Fecundación in vitro, no es
+            necesario la extracción previa de los óvulos de la mujer.
+          </p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/reproduccion-asistida/reproduccion-3.png",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 0,
+    description_2: "Procedimiento",
+  },
+  {
+    icon: "/src/assets/salud-mujer/reproduccion-asistida/icon-fecundacion.png",
+    title: "Inseminación Artificial (IA)",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 0,
+    description_2: ``,
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/reproduccion-asistida/reproduccion-4.png",
+    imageDesc: "",
+    description_1: `
+    <h4>¿Qué es la Fecundación In Vitro?</h4>
+    <p>La Fecundación In Vitro (FIV) es la unión del óvulo con el espermatozoide en el laboratorio-in vitro-, con el fin de obtener embriones ya fecundados para transferir al útero materno y que evolucionen hasta conseguir un embarazo.</p>
+    <p>
+      La fecundación de los óvulos puede llevarse a cabo mediante técnica de FIV convencional o Inyección Intracitoplasmática de Espermatozoides (ICSI). Es la técnica de Reproducción Asistida más utilizada en las clínicas de Medicina Reproductiva pero será el ginecólogo el que determine, tras un estudio previo sobre las posibles casusas de la infertilidad, cuál es la más adecuada para cada caso.
+    </p>`,
+    orientation: 1,
+    description_2: `<p>La Fecundación In Vitro (FIV) es la unión del óvulo con el espermatozoide en el laboratorio-in vitro-, con el fin de obtener embriones ya fecundados para transferir al útero materno y que evolucionen hasta conseguir un embarazo.</p>`,
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/reproduccion-asistida/reproduccion-5.png",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 0,
+    description_2: "Procedimiento",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/reproduccion-asistida/reproduccion-6.png",
+    imageDesc: "",
+    description_1: `<h4>¿Qué es la Ovodonación?</h4>
+                <p>
+                    La ovodonación es un tratamiento de Reproducción Asistida que ofrece la oportunidad de tener un hijo a pacientes que no pueden gestar un embrión por sí mismas. En una ovodonación, se realiza una FIV (Fecundación in vitro) con la peculiaridad que los óvulos son de una donante y los espermatozoides son de la pareja receptora. También puede existir el caso, si se trata de mujeres que afrontan la maternidad en solitario, que el esperma también sea de donante.
+                </p><br>
+                <p>
+                    Así, la donación de óvulos hace posible el milagro de la vida en mujeres que de otra manera no podrían tener hijos.
+                </p>`,
+    orientation: 0,
+    description_2: `<p>
+            La ovodonación permite dar la oportunidad a mujeres que no pueden
+            tener hijos de gestar un embrión por sí mismas, fruto de los óvulos
+            de una donante y los espermatozoides de la pareja receptora, en caso
+            de tenerla, teniendo la opción de recurrir al banco de semen en caso
+            contrario.
+          </p>`,
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc: "",
+    imageDesc: "",
+    description_1: `<h4>La donante</h4>
+          <p>
+            La donación de óvulos fue autorizada por la legislación española en
+            el año 1988 (Ley 35/1988,22 de Noviembre) Desde ese momento, la
+            donación está regulada y establece que la donante tiene que cumplir
+            una serie de requisitos para donar óvulos:
+          </p>`,
+    orientation: 0,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/reproduccion-asistida/reproduccion-7.png",
+    imageDesc: "",
+    description_1: `<h4 class="pb-2">Edad: 18 a 35 años</h4>
+                <p>
+                  Historial genético acorde a los requisitos de la legislación
+                  vigente.<br />
+                  Normalidad del aparato reproductor.<br />
+                  Evaluación ginecológica completa.<br />
+                  Evaluación psicológica completa.<br />
+                </p>`,
+    orientation: 1,
+    description_2: "",
+  },
+  {
+    icon: "",
+    title: "",
+    imageSrc:
+      "/src/assets/salud-mujer/reproduccion-asistida/reproduccion-8.png",
+    imageDesc: "",
+    description_1: ``,
+    orientation: 2,
+    description_2: `<p>
+            Las mujeres se someten a una evaluación clínica y psicológica
+            completa que nos ofrece una valiosa información respecto a su
+            fertilidad, su estado de salud y su potencial para ser madres. Se
+            les realiza una exploración ginecológica completa que permite
+            descartar la presencia de quistes, miomas, pólipos y otras
+            alteraciones del aparato reproductor. Además, se le realiza un
+            estudio cromosómico que descarta futuras anomalías en bebés propios
+            o en las receptoras de ovocitos. Se confirma la ausencia de
+            enfermedades transmisibles como el VIH, la hepatitis, sífilis y
+            también el grupo sanguíneo y Rh.
+          </p><br />
+          <p>
+            Según la Ley de técnicas de reproducción asistida, toda donación de
+            óvulos es anónima y voluntaria, no se revela la identidad de
+            donantes ni de las receptoras. No obstante, se puede facilitar
+            aquella información de interés para un correcto seguimiento de la
+            gestación, como el grupo sanguíneo y la edad de la donante.
+          </p>`,
+  },
+];
